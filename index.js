f81d947ddef4f1da9f664eaaafb927af3d3b4b35
Vinicius Sossella <viniciussossella@gmail.com>
2016-07-16 12:25:35 -0300

    login

--- index.js
+++ index.js
@@ -33,6 +33,34 @@ api.post('/user', function (request) {
 }, { success: 201 }); // Return HTTP status 201 - Created when successful
 
 
+// Create new user
+api.post('/user/login', function (request) {
+	'use strict';
+
+	var email, senha, params;
+
+	// Get the id from the pathParams
+	email = request.body.email;
+    senha = request.body.senha;
+	
+	// Set up parameters for dynamo
+	params = {
+		TableName: 'usuario',
+		FilterExpression: 'email = :email and password = :senha',
+		ExpressionAttributeValues: {
+			':email': email,
+			':senha': senha
+		}
+	};
+
+	// Store it and return the promise,
+	// that will evaluate before reponding back to the client
+	return docClient.scanAsync(params);
+
+}, { success: 201 }); // Return HTTP status 201 - Created when successful
+
+
+
 // get user for {id}
 api.get('/user/{id}', function (request) {
 	'use strict';
